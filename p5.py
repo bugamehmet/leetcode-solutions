f74136fb82b4d2a03f2969e550946a6f57c9751f
bugamehmet <bugamehmetcan@gmail.com>
2023-12-07 10:53:55 +0300

    p5-py

--- p5.py
+++ p5.py
@@ -2,17 +2,19 @@
 
 words = ["cat","bt","hat","tree"]
 chars = "atach"
+words2 = ["hello","world","leetcode"]
+chars2 = "welldonehoneyr"
 
+"""
+for kelime in kelimeler: döngüsü ile kelimeler listesindeki her bir kelimeye tek tek erişiliyor.
+set(kelime) kullanılarak kelimenin içindeki benzersiz karakterlere erişiliyor. Bu adım, her bir karakterin sayısını sayarken tekrarları önlemeye yardımcı olur.
+all(kelime.count(char) <= karakterler.count(char) for char in set(kelime)): ifadesi, kelimenin her karakteri için kontrol ettiği bir şartı içerir. Bu şart, kelimenin her bir karakterinin, karakterler dizesindeki aynı karakterin sayısından küçük veya eşit olup olmadığını kontrol eder.
+Eğer bu şart, kelimenin her karakteri için sağlanıyorsa (yani, all fonksiyonu True döndürüyorsa), o zaman bu kelimenin uzunluğu toplam_uzunluk değişkenine eklenir (toplam_uzunluk += len(kelime)).
 
-dizi = []
-for j in words:
-    dizi.append(j)
-print(dizi)
+"""
 
-sayac = 0
-for k in chars:
-    print(k)
-    if k in dizi:
-        sayac += 1
-
-print(sayac)
+top = 0
+for word in words:
+    if all(word.count(char) <= chars.count(char) for char in set(word)):
+        top += len(word)
+print(top)
