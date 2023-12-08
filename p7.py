9b74ae5eb371ba154bea72953b001a8540280973
bugamehmet <bugamehmetcan@gmail.com>
2023-12-08 14:55:33 +0300

    p7-py

---
+++ p7.py
@@ -0,0 +1,13 @@
+def tree2str(self, root):
+    if not root:
+        return ""
+
+    result = str(root.val)
+
+    if root.left or root.right:
+        result += "(" + self.tree2str(root.left) + ")"
+
+    if root.right:
+        result += "(" + self.tree2str(root.right) + ")"
+
+    return result
