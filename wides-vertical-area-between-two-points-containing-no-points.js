045d1b3a513cab43958a5360ad1998c8639a7bff
bugamehmet <bugamehmetcan@gmail.com>
2023-12-21 17:33:34 +0300

    py-js

---
+++ wides-vertical-area-between-two-points-containing-no-points.js
@@ -0,0 +1,11 @@
+var maxWidthOfVerticalArea = function(points) {
+  points.sort((a, b) => a[0] - b[0]);
+  let maxVertical = 0;
+
+  for (let i = 1; i < points.length; i++) {
+      let width = points[i][0] - points[i - 1][0];
+      maxVertical = Math.max(maxVertical, width);
+  }
+
+  return maxVertical;
+};
