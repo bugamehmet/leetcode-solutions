3efdbd0f7f5138b9d1936f334723cba3f020563e
bugamehmet <bugamehmetcan@gmail.com>
2024-01-31 10:01:22 +0300

    ğy

---
+++ daily-temperatures.py
@@ -0,0 +1,15 @@
+from collections import deque
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        answer = [0] * n
+        stack = deque()
+
+        for i in range(n):
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                prev_day = stack.pop()
+                answer[prev_day] = i - prev_day
+            stack.append(i)
+
+        return answer
